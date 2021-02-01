18470c652bc34938fc75202c1b84cd628bf9becb
anthony <jauch-anthony@hotmail.fr>
2021-02-01 23:14:30 +0100

    delete movie cell

---
+++ PimpMyPlaylist/Structures/UpdateWatchListMovie.swift
@@ -0,0 +1,17 @@
+//
+//  ResultWatchListMovie.swift
+//  PimpMyPlaylist
+//
+//  Created by Anthony Jauch on 30/01/2021.
+//
+
+import Foundation
+
+struct UpdateWatchListMovie: Codable{
+    var movies: [Int]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies
+        
+    }
+}
